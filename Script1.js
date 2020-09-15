13b0b8cc53e697850ea30dc21bc051de477ae769
Joshua Thompson <jrt160530@outlook.com>
2020-09-15 12:44:50 -0500

    Add project files.

---
+++ Script1.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+'use strict'
+
+let today = new Date();
+let formatDate = today.toDateString();
+let selectElement = document.getElementById('date');
+selectElement.innerHTML = formatDate;
+
+console.log('Here\'s a console message');
